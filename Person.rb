e41d7c90a6e49b92cece975afd6b601ab5df73dc
Dilsher <dilsherb@gmail.com>
2023-07-24 16:47:43 +0300

    initializer defined.

--- Person.rb
+++ Person.rb
@@ -1,2 +1,9 @@
 class Person
     attr_accessor :id, :name, :age
+    
+  def initialize(name = "Unknown", age, parent_permission: true)
+    @id = Random.rand(1..10_000)
+    @name = name
+    @age = age
+    @parent_permission = true
+  end
