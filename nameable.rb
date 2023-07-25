a2c97745eb9dcd694822a92b433c0f8d7f365430
Dilsher Balouch <dilsherb@gmail.com>
2023-07-25 17:54:00 +0300

    correct_name method completed.

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
   def correct_name
-    raise NotImplementedError "#{this.class} has not implemented '#{__method__}'"
+    raise NotImplementedError "#{this.class} has not implemented method '#{__method__}'"
   end
 end
