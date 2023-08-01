4811b2a624c089d205900be5526b72d546d36336
Gakii Beryl <gakiiberyl@gmail.com>
2023-08-01 00:57:44 +0000

    Update options.rb for creating person and book

--- app/options.rb
+++ app/options.rb
@@ -31,14 +31,21 @@ def list_all_people(app)
 end
 
 def create_person(app)
-  print "1 to create a student\n2 to create a teacher\n [Input the number]:"
+  print "1 to create a student\n2 to create a teacher\n [Input the number]: "
   choice = gets.chomp.to_i
 
   case choice
   when 1
-    app.create_student
+    age = get_integer_input('Age: ')
+    name = get_string_input('Name: ')
+    parent_permission = get_boolean_input('Has parent permission? [Y/N]: ')
+    classroom = get_string_input('Classroom: ')
+    app.create_student(age, name, parent_permission, classroom)
   when 2
-    app.create_teacher
+    age = get_integer_input('Age: ')
+    name = get_string_input('Name: ')
+    specialization = get_string_input('Specialization: ')
+    app.create_teacher(age, name, specialization)
   else
     puts 'Invalid choice. Please try again.'
     create_person(app)
@@ -46,15 +53,42 @@ def create_person(app)
 end
 
 def create_book(app)
-  app.create_book
+  title = get_string_input('Title: ')
+  author = get_string_input('Author: ')
+  app.create_book(title, author)
 end
 
 def create_rental(app)
-  app.create_rental
+  app.display_books
+  if app.books.empty?
+    puts 'No books available to rent'
+  else
+    puts 'Choose a book from the following list.'
+    book_index = get_integer_input('Enter the index of the book: ')
+    selected_book = app.books[book_index]
+
+    app.display_people
+    if app.people.empty?
+      puts 'No persons created. Kindly create a person before renting'
+    else
+      puts 'Choose a person from the following list.'
+      person_index = get_integer_input('Enter the index of the person: ')
+      selected_person = app.people[person_index]
+
+      date = get_string_input('Date: ')
+      app.create_rental(date, selected_book, selected_person)
+    end
+  end
 end
 
 def list_rentals_of_person(app)
-  app.list_rentals_of_person
+  app.display_people
+  if app.people.empty?
+    puts 'No persons created.'
+  else
+    person_id = get_integer_input('Enter the ID of the person: ')
+    app.list_rentals_of_person(person_id)
+  end
 end
 
 def exit_app(_)
