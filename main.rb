25bd859d5404ddc4db3e59bc2e6628096c96e69c
Gakii Beryl <gakiiberyl@gmail.com>
2023-08-01 01:18:02 +0000

    Correcting linters error

--- main.rb
+++ main.rb
@@ -13,8 +13,7 @@ end
 
 def get_boolean_input(prompt)
   print prompt
-  input = gets.chomp.downcase
-  input != 'n'
+  gets.chomp.downcase != 'n'
 end
 
 def create_student(app)
@@ -40,75 +39,75 @@ end
 
 def create_rental(app)
   app.display_books
-  if app.books.empty?
-    puts 'No books available to rent'
-  else
-    puts 'Choose a book from the following list.'
-    book_index = get_integer_input('Enter the index of the book: ')
-    selected_book = app.books[book_index]
+  return puts 'No books available to rent' if app.books.empty?
 
-    app.display_people
-    if app.people.empty?
-      puts 'No persons created. Kindly create a person before renting'
-    else
-      puts 'Choose a person from the following list.'
-      person_index = get_integer_input('Enter the index of the person: ')
-      selected_person = app.people[person_index]
+  puts 'Choose a book from the following list.'
+  book_index = get_integer_input('Enter the index of the book: ')
+  selected_book = app.books[book_index]
 
-      date = get_string_input('Date: ')
-      app.create_rental(date, selected_book, selected_person)
-    end
-  end
+  app.display_people
+  return puts 'No persons created. Kindly create a person before renting' if app.people.empty?
+
+  puts 'Choose a person from the following list.'
+  person_index = get_integer_input('Enter the index of the person: ')
+  selected_person = app.people[person_index]
+
+  date = get_string_input('Date: ')
+  app.create_rental(date, selected_book, selected_person)
 end
 
 def list_rentals_of_person(app)
   app.display_people
-  if app.people.empty?
-    puts 'No persons created.'
+  return puts 'No persons created.' if app.people.empty?
+
+  person_id = get_integer_input('Enter the ID of the person: ')
+  app.list_rentals_of_person(person_id)
+end
+
+def print_welcome_message
+  puts 'Welcome to the School Library App!'
+end
+
+def print_menu
+  puts "\nPlease choose an option by entering a number:"
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create student'
+  puts '4 - Create teacher'
+  puts '5 - Create book'
+  puts '6 - Create rental'
+  puts '7 - List rentals of person'
+  puts '8 - Exit app'
+end
+
+def handle_menu_option(app, number)
+  menu_options = {
+    1 => :list_all_books,
+    2 => :list_all_people,
+    3 => :create_student,
+    4 => :create_teacher,
+    5 => :create_book,
+    6 => :create_rental,
+    7 => :list_rentals_of_person,
+    8 => :exit_app
+  }
+
+  if menu_options.key?(number)
+    send(menu_options[number], app)
   else
-    person_id = get_integer_input('Enter the ID of the person: ')
-    app.list_rentals_of_person(person_id)
+    puts 'Invalid option. Please try again.'
   end
 end
 
 def main
   app = App.new
-  puts 'Welcome to the School Library App!'
+  print_welcome_message
 
   loop do
-    puts "\nPlease choose an option by entering a number:"
-    puts "1 - List all books"
-    puts "2 - List all people"
-    puts "3 - Create student"
-    puts "4 - Create teacher"
-    puts "5 - Create book"
-    puts "6 - Create rental"
-    puts "7 - List rentals of person"
-    puts "8 - Exit app"
+    print_menu
+    number = get_integer_input('')
 
-    number = gets.chomp.to_i
-
-    case number
-    when 1
-      app.list_all_books
-    when 2
-      app.list_all_people
-    when 3
-      create_student(app)
-    when 4
-      create_teacher(app)
-    when 5
-      create_book(app)
-    when 6
-      create_rental(app)
-    when 7
-      list_rentals_of_person(app)
-    when 8
-      puts 'Thank you for using this app!'
-      break
-    else
-      puts 'Invalid option. Please try again.'
-    end
+    handle_menu_option(app, number)
   end
 end
 
