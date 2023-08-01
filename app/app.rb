f07042b16258070663fd14e9a5d0abb62286071b
Dilsher <dilsherb@gmail.com>
2023-08-01 16:42:16 +0300

    refactor the data

--- app/app.rb
+++ app/app.rb
@@ -4,6 +4,7 @@ require_relative '../classes/student'
 require_relative '../classes/teacher'
 require_relative '../classes/book'
 require_relative '../classes/rental'
+require_relative 'user_input'
 
 class App
   attr_accessor :books, :people
@@ -13,60 +14,109 @@ class App
     @people = []
   end
 
+  # List all books in the library
   def list_all_books
     if @books.empty?
       puts 'No books available'
     else
-      @books.each_with_index { |book, index| puts "#{index}) 📚 Title: '#{book.title}', Author: '#{book.author}'" }
+      @books.each_with_index { |book, index| puts "#{index+1}): 📚 Title: '#{book.title}', Author: #{book.author}" }
     end
   end
 
+  # List all people (students and teachers) in the library
   def list_all_people
     if @people.empty?
       puts 'No person created yet'
     else
       @people.each_with_index do |person, index|
-        info = "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-        if person.is_a?(Teacher)
-          info += " Specialization: #{person.specialization}"
-        elsif person.is_a?(Student)
-          info += " Classroom: #{person.classroom}"
-        end
+        info = "#{index+1}): [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+        info += ", Specialization: #{person.specialization}" if person.is_a?(Teacher)
+        info += ", Classroom: #{person.classroom}" if person.is_a?(Student)
         puts info
       end
     end
   end
 
-  def create_student(age, name, parent_permission, classroom)
-    puts "🎉 Student '#{name}' created successfully"
-    @people << Student.new(age, classroom, name, parent_permission: parent_permission)
+  # Prompt the user to create a person (student or teacher)
+  def create_person
+    choice = get_user_input('Do you want to create a student(1) or a teacher(2)? [Input the number]:', :integer)
+
+    case choice
+    when 1
+      create_student
+    when 2
+      create_teacher
+    else
+      puts 'Invalid choice. Please try again.'
+      create_person
+    end
   end
 
-  def create_teacher(age, name, specialization)
-    puts "🎉 Teacher '#{name}' created successfully"
-    @people << Teacher.new(age, specialization, name)
+  # Prompt the user to create a student with required details and add to the people array
+  def create_student
+    age = get_user_input('Age: ', :integer)
+    name = get_user_input('Name: ', :string)
+    parent_permission = get_user_input('Has parent permission? [Y/N]: ', :boolean)
+    classroom = get_user_input('Classroom: ', :string)
+    student = Student.new(age, classroom, name, parent_permission: parent_permission)
+    puts "🎉 Student #{name} created successfully"
+    @people << student
   end
 
-  def create_book(title, author)
-    puts "🎉 Book '#{title}' created successfully"
-    @books << Book.new(title, author)
+  # Prompt the user to create a teacher with required details and add to the people array
+  def create_teacher
+    age = get_user_input('Age: ', :integer)
+    name = get_user_input('Name: ', :string)
+    specialization = get_user_input('Specialization: ', :string)
+    teacher = Teacher.new(age, specialization, name)
+    puts "🎉 Teacher #{name} created successfully"
+    @people << teacher
   end
 
-  def create_rental(date, selected_book, selected_person)
-    Rental.new(date, selected_book, selected_person)
-    puts '🎉 Rental created successfully'
+  # Prompt the user to create a book with required details and add to the books array
+  def create_book
+    title = get_user_input('Title: ', :string)
+    author = get_user_input('Author: ', :string)
+    book = Book.new(title, author)
+    puts "🎉 Book #{title} created successfully"
+    @books << book
   end
 
+  # Prompt the user to create a rental for a book and a person
+  def create_rental
+    if @books.empty?
+      puts 'No books available to rent'
+    else
+      display_books
+      if @people.empty?
+        puts 'No persons created. Kindly create a person before renting'
+      else
+        display_people
+        date = get_user_input('Date: ', :string)
+        Rental.new(date, @selected_book, @selected_person)
+        puts '🎉 Rental created successfully'
+      end
+    end
+  end
+
+  # Display all books with corresponding index and store user-selected book
   def display_books
     list_all_books
+    index = get_user_input('Select a book by number: ', :integer)
+    @selected_book = @books[index-1]
   end
 
+  # Display all people with corresponding index and store user-selected person
   def display_people
     list_all_people
+    index = get_user_input('Select a person by number: ', :integer)
+    @selected_person = @people[index-1]
   end
 
-  def list_rentals_of_person(id)
-    person_selected = @people.select { |person| person.id == id }.first
+  # List rentals for a specific person by their ID
+  def list_rentals_of_person
+    id = get_user_input('ID of Person: ', :integer)
+    person_selected = @people.find { |person| person.id == id }
     if person_selected.nil?
       puts 'No such person exists'
     else
--- main.rb
+++ main.rb
@@ -1,126 +1,18 @@
+# Include the required files
 require_relative 'app/app'
 require_relative 'app/options'
 
-def get_integer_input(prompt)
-  print prompt
-  gets.chomp.to_i
-end
-
-def get_string_input(prompt)
-  print prompt
-  gets.chomp
-end
-
-def get_boolean_input(prompt)
-  print prompt
-  gets.chomp.downcase != 'n'
-end
-
-def create_student(app)
-  age = get_integer_input('Age: ')
-  name = get_string_input('Name: ')
-  parent_permission = get_boolean_input('Has parent permission? [Y/N]: ')
-  classroom = get_string_input('Classroom: ')
-  app.create_student(age, name, parent_permission, classroom)
-end
-
-def create_teacher(app)
-  age = get_integer_input('Age: ')
-  name = get_string_input('Name: ')
-  specialization = get_string_input('Specialization: ')
-  app.create_teacher(age, name, specialization)
-end
-
-def create_person(app)
-  puts 'Do you want to create a student (1) or a teacher (2)?'
-  choice = get_integer_input('Enter the number: ')
-
-  case choice
-  when 1
-    create_student(app)
-  when 2
-    create_teacher(app)
-  else
-    puts 'Invalid choice. Please try again.'
-  end
-end
-
-def create_book(app)
-  title = get_string_input('Title: ')
-  author = get_string_input('Author: ')
-  app.create_book(title, author)
-end
-
-def create_rental(app)
-  app.display_books
-  return puts 'No books available to rent' if app.books.empty?
-
-  puts 'Choose a book from the following list.'
-  book_index = get_integer_input('Enter the index of the book: ')
-  selected_book = app.books[book_index]
-
-  app.display_people
-  return puts 'No persons created. Kindly create a person before renting' if app.people.empty?
-
-  puts 'Choose a person from the following list.'
-  person_index = get_integer_input('Enter the index of the person: ')
-  selected_person = app.people[person_index]
-
-  date = get_string_input('Date: ')
-  app.create_rental(date, selected_book, selected_person)
-end
-
-def list_rentals_of_person(app)
-  app.display_people
-  return puts 'No persons created.' if app.people.empty?
-
-  person_id = get_integer_input('Enter the ID of the person: ')
-  app.list_rentals_of_person(person_id)
-end
-
-def print_welcome_message
-  puts 'Welcome to the School Library App!'
-end
-
-def print_menu
-  puts "\nPlease choose an option by entering a number:"
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create person'
-  puts '4 - Create book'
-  puts '5 - Create rental'
-  puts '6 - List rentals of person'
-  puts '7 - Exit app'
-end
-
-def handle_menu_option(app, number)
-  menu_options = {
-    1 => :list_all_books,
-    2 => :list_all_people,
-    3 => :create_person,
-    4 => :create_book,
-    5 => :create_rental,
-    6 => :list_rentals_of_person,
-    7 => :exit_app
-  }
-
-  if menu_options.key?(number)
-    send(menu_options[number], app)
-  else
-    puts 'Invalid option. Please try again.'
-  end
-end
-
+# The main function of the application.
 def main
+  # Create an instance of the App class
   app = App.new
-  print_welcome_message
 
-  loop do
-    print_menu
-    number = get_integer_input('')
+  # Display a welcome message to the user
+  puts 'Welcome to the School Library App!'
 
-    handle_menu_option(app, number)
-  end
+  # Call the options method to present the menu and handle user inputs
+  options(app)
 end
 
+# Call the main function to start the application
 main
