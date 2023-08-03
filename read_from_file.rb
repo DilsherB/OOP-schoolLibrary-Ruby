f53bc3f29540b7a9b0fc98b0ed2792ddb96f9237
Dilsher Balouch <dilsherb@gmail.com>
2023-08-03 19:07:22 +0300

    read_from_file.rb completed.

--- read_from_file.rb
+++ read_from_file.rb
@@ -8,52 +8,11 @@
 #   puts "File 'book.json' doesn't exist"
 # end
 
-# def get_data(filename)
-#   json_array = []
-#   if File.exist?(filename)
-#     file_data = File.read(filename)
-#     json_array = JSON.parse(file_data)
-#   else
-#     json_array = []
-#   end
-#   return json_array
-# end
-
-# def show_data(filename)
-#   json_array = get_data(filename)
-#   puts "Data from file #{filename}:"
-#   if json_array.empty?
-#     puts 'File is empty'
-#   else
-#     json_array.each do |data|
-#       puts data.keys.join(': ')
-#       puts data.values.join(', ')
-#       puts
-#     end
-#   end
-# end
-
-def get_data(filename)
-  json_array = []
+def read_from_file(filename)
   if File.exist?(filename)
-    file_data = File.read(filename)
-    json_array = JSON.parse(file_data)
+    data = JSON.parse(File.read(filename))
   else
-    json_array = []
+    data = []
   end
-  json_array
-end
-
-def show_data(filename)
-  json_array = get_data(filename)
-  puts "Data from file #{filename}:"
-  if json_array.empty?
-    puts 'File is empty'
-  else
-    json_array.each do |data|
-      puts data.keys.join(': ')
-      puts data.values.join(', ')
-      puts
-    end
-  end
+  data
 end
